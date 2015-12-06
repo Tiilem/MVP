30fe152104c9e36034f3150467ae9de7dcf966a7
上海－郭朝阳 <guochaoyang@jd.com>
2015-12-06 11:29:11 +0800

    Initial Commit

---
+++ MVP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVP
+//
+//  Created by ccguo on 15/12/6.
+//  Copyright © 2015年 ccguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVP
+//
+//  Created by ccguo on 15/12/6.
+//  Copyright © 2015年 ccguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
