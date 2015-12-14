fcc63077eb48cb47f0aed24d94ccd1f3417c40db
上海－郭朝阳 <guochaoyang@jd.com>
2015-12-14 23:33:46 +0800

    。。

---
+++ MVP/Action/MUAction.h
@@ -0,0 +1,13 @@
+//
+//  MUAction.h
+//  MVP
+//
+//  Created by ccguo on 15/12/14.
+//  Copyright © 2015年 ccguo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MUAction : NSObject
+
+@end
